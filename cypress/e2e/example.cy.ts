668747f09d2a8d3be2eb829ee9f4f9494af635b1
codydhayden <cody.hayden@applitools.com>
2023-06-01 13:56:53 -0500

    cypress 10 changes

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,10 +1,12 @@
+/// <reference types="@applitools/eyes-cypress" />
+
 describe('example', () => {
   it('should pass', () => {
-        cy.visit('https://www.google.com');
+        cy.visit('https://www.example.com');
 
         cy.eyesOpen({
             appName: 'Cypress Test',
-            testName: 'Cypress Test',
+            testName: 'Cypress Test'
         })
 
         cy.eyesCheckWindow({
